8c6ef99a7555a787fadf2e24c17b84f58bc62efe
Enrique Alonso Barreiro <enr.barreiro@gmail.com>
2025-10-07 15:14:01 +0200

    Added particles while moving

---
+++ Playable Character/PlayerMovementParticles.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerMovementParticles : MonoBehaviour
+{
+    public PcMovement playerMovement;
+    public List<GameObject> movementParticlesUp = new();
+    public List<GameObject> movementParticlesBack = new();
+    public List<GameObject> movementParticlesLeft = new();
+    public List<GameObject> movementParticlesRight = new();
+
+    List<GameObject> activeParticles = new();
+    string lastDir = "";
+    readonly string[] movementGroupUp = new string[] { "Up", "RightUp", "LeftUp" };
+    readonly string[] movementGroupDown = new string[] { "Down", "RightDown", "LeftDown" };
+
+
+    void Update()
+    {
+        if (playerMovement == null) return;
+
+        string dir = playerMovement.lastMov;
+
+        if (dir != lastDir)
+        {
+            if (movementGroupUp.Contains(lastDir) && movementGroupUp.Contains(dir)) return;
+            else if (movementGroupDown.Contains(lastDir) && movementGroupDown.Contains(dir)) return;
+            else lastDir = dir;
+        }
+        else return;
+
+        switch (dir)
+        {
+            case "Up":
+            case "RightUp":
+            case "LeftUp":
+                EnableParticleEffect(movementParticlesUp);
+                break;
+
+            case "Down":
+            case "RightDown":
+            case "LeftDown":
+                EnableParticleEffect(movementParticlesBack);
+                break;
+
+            case "Left":
+                EnableParticleEffect(movementParticlesLeft);
+                break;
+
+            case "Right":
+                EnableParticleEffect(movementParticlesRight);
+                break;
+
+            default:
+                DisableActiveParticles();
+                break;
+        }
+
+    }
+
+    void DisableActiveParticles()
+    {
+        if (!activeParticles.Any()) return;
+
+        foreach (GameObject particle in activeParticles)
+        {
+            particle.SetActive(false);
+        }
+    }
+
+    void EnableParticleEffect(List<GameObject> particlesToEnable)
+    {
+        DisableActiveParticles();
+        foreach (GameObject particle in particlesToEnable)
+        {
+            particle.SetActive(true);
+            activeParticles.Add(particle);
+        }
+    }
+}

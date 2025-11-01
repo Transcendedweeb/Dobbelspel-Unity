62bcccfe02e298498a078188cbc3c3761798a493
Enrique Alonso Barreiro <enr.barreiro@gmail.com>
2025-11-01 13:38:07 +0100

    Updated timings and cd's

--- Bosses/BossAI.cs
+++ Bosses/BossAI.cs
@@ -1,69 +1,144 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class AttackData
+{
+    public GameObject attackObject;
+    public float cooldownAfter = 1f;
+}
+
+[System.Serializable]
+public class AttackSequence
+{
+    public string name;
+    public float startCooldown = 0f;
+    public AttackData[] attacks;
+}
+
+[System.Serializable]
+public class SpecialAttack
+{
+    public string name;
+    public AttackData[] attacks;
+    public float[] hpThresholds;
+    [HideInInspector] public bool[] triggered;
+}
+
+[RequireComponent(typeof(HealthManager))]
 public class BossAI : MonoBehaviour
 {
-    public GameObject[] attacks;
+    [Header("Attack Settings")]
+    public AttackSequence[] attackSequences;
+    public SpecialAttack specialAttack;
     public GameObject melee;
-    public GameObject special;
     public GameObject player;
     public float closeDistance = 5f;
-    public float attackCooldown = 0f;
-    public float starWaitTime = 0f;
+    public float startWaitTime = 0f;
+
     [HideInInspector] public bool attacking = true;
-    int count;
-    bool specialActivated = false;
-    HealthManager healthManager;
+
+    private int currentSequenceIndex = 0;
+    private int currentAttackIndex = 0;
+    private HealthManager healthManager;
+    private bool sequenceStarting = false;
+    private AttackData currentAttack;
 
     void Start()
     {
-        Invoke("InvokeReset", starWaitTime);
-        count = attacks.Length-1;
-        healthManager = this.gameObject.GetComponent<HealthManager>();
+        healthManager = GetComponent<HealthManager>();
+        if (specialAttack != null)
+        {
+            specialAttack.triggered = new bool[specialAttack.hpThresholds.Length];
+        }
+        Invoke(nameof(InvokeReset), startWaitTime);
     }
 
     void Update()
     {
-        if (!attacking)
+        if (!attacking && !sequenceStarting)
         {
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
-            if ((special != null) && !specialActivated)
+            if (specialAttack != null)
             {
-                if (healthManager.health < (healthManager.maxHealth/2))
+                for (int i = 0; i < specialAttack.hpThresholds.Length; i++)
                 {
-                    specialActivated = true;
-                    special.SetActive(true);
-                    attacking = true;
-                    return;
+                    if (!specialAttack.triggered[i] && healthManager.health <= specialAttack.hpThresholds[i])
+                    {
+                        specialAttack.triggered[i] = true;
+                        StartCoroutine(PerformAttackSequence(specialAttack.attacks));
+                        return;
+                    }
                 }
             }
 
             if (distanceToPlayer <= closeDistance && melee != null)
             {
                 melee.SetActive(true);
+                attacking = true;
             }
-            else
+            else if (attackSequences.Length > 0)
             {
-                if (count >= attacks.Length) count = 0;
+                AttackSequence sequence = attackSequences[currentSequenceIndex];
 
-                attacks[count].SetActive(true);
-                count++;
+                if (currentAttackIndex >= sequence.attacks.Length)
+                {
+                    currentAttackIndex = 0;
+
+                    if (attackSequences.Length > 1)
+                    {
+                        int nextIndex;
+                        do
+                        {
+                            nextIndex = Random.Range(0, attackSequences.Length);
+                        } while (nextIndex == currentSequenceIndex);
+
+                        currentSequenceIndex = nextIndex;
+                    }
+
+                    return;
+                }
+
+                currentAttack = sequence.attacks[currentAttackIndex];
+                currentAttackIndex++;
+                attacking = true;
+                currentAttack.attackObject.SetActive(true);
             }
-            attacking = true;
         }
-
     }
 
-    void ResetAttacking()
+    IEnumerator PerformAttackSequence(AttackData[] sequence)
     {
-        attacking = false;
+        foreach (AttackData attack in sequence)
+        {
+            currentAttack = attack;
+            attack.attackObject.SetActive(true);
+            attacking = true;
+
+            yield return new WaitUntil(() => !attacking);
+
+            yield return new WaitForSeconds(attack.cooldownAfter);
+        }
+
+        InvokeReset();
     }
 
     public void InvokeReset()
     {
-        Invoke("ResetAttacking", attackCooldown);
+        if (currentAttack != null)
+        {
+            StartCoroutine(WaitAfterAttack(currentAttack.cooldownAfter));
+        }
+        else
+        {
+            attacking = false;
+        }
+    }
+
+    IEnumerator WaitAfterAttack(float cooldown)
+    {
+        yield return new WaitForSeconds(cooldown);
+        attacking = false;
     }
 }
-

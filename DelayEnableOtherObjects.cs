5e0246aa1022750b80a79e7efc3b8a8b64db78bd
Enrique Alonso Barreiro <enr.barreiro@gmail.com>
2025-12-01 23:07:49 +0100

    added option for child gameobjects as list

--- DelayEnableOtherObjects.cs
+++ DelayEnableOtherObjects.cs
@@ -11,7 +11,14 @@ public class DelayEnableOtherObjects : MonoBehaviour
         public float delay;
     }
 
+    [Header("Enable Listed Objects")]
     public List<DelayedObject> objectsToEnable = new();
+
+    [Header("Children Enable Mode")]
+    public bool useChildrenInstead = false;
+    public bool reverseChildrenOrder = false;
+    public float startDelay = 0f;
+    public float childDelay = 0.2f;
 
     private void OnEnable()
     {
@@ -20,6 +27,43 @@ public class DelayEnableOtherObjects : MonoBehaviour
 
     IEnumerator EnableObjectsRoutine()
     {
+        if (startDelay > 0f)
+            yield return new WaitForSeconds(startDelay);
+
+        if (useChildrenInstead)
+        {
+            int childCount = transform.childCount;
+
+            if (!reverseChildrenOrder)
+            {
+                for (int i = 0; i < childCount; i++)
+                {
+                    var child = transform.GetChild(i).gameObject;
+
+                    if (child != null)
+                        child.SetActive(true);
+
+                    if (childDelay > 0f)
+                        yield return new WaitForSeconds(childDelay);
+                }
+            }
+            else
+            {
+                for (int i = childCount - 1; i >= 0; i--)
+                {
+                    var child = transform.GetChild(i).gameObject;
+
+                    if (child != null)
+                        child.SetActive(true);
+
+                    if (childDelay > 0f)
+                        yield return new WaitForSeconds(childDelay);
+                }
+            }
+
+            yield break;
+        }
+
         foreach (var item in objectsToEnable)
         {
             if (item == null || item.targetObject == null)
